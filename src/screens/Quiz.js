3eed3add7fb612b767b90fd3b5c5ce757bd6bff5
RayanCardoso <rayancardoso40@gmail.com>
2025-03-30 18:57:29 -0300

    feature: adicionando tela de Quiz

---
+++ src/screens/Quiz.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+const Quiz = () => {
+  return (
+    <LinearGradient colors={['#59A8DA', '#FFFFFF']} style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image source={require('../../assets/icons_quiz.png')} style={styles.iconMath} />
+        <Image source={require('../../assets/faeterj_logo.png')} style={styles.logo} />
+      </View>
+      <View style={styles.optionsContainer}>
+        <TouchableOpacity style={styles.optionButton}>
+          <Text style={styles.optionText}>5PDM</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.optionButton}>
+          <Text style={styles.optionText}>4UBD</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.optionButton}>
+          <Text style={styles.optionText}>4POA</Text>
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#E3F2FD',
+  },
+  iconMath: {
+    height: 60,
+    resizeMode: "contain",
+    marginBottom: 10,
+  },
+  logo: {
+    height: 170,
+    resizeMode: "contain",
+  },
+  logoContainer: {
+    alignItems: 'center',
+    justifyContent: "center",
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height / 2 - 100,
+  },
+  optionsContainer: {
+    width: '80%',
+    alignItems: 'center',
+  },
+  optionButton: {
+    backgroundColor: '#1565C0',
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 10,
+    marginVertical: 10,
+    width: '100%',
+    alignItems: 'center',
+  },
+  optionText: {
+    color: '#FFF',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default Quiz;
